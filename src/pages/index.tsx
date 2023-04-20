bcaef47c2425d2eae3ba835a064d066dd6733f20
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-20 13:33:45 -0300

    adjusts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Aside } from "@/components/Aside";
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
-import { Main } from "@/components/Main";
+import { Aside } from "@components/Aside";
+import { Footer } from "@components/Footer";
+import { Header } from "@components/Header";
+import { Main } from "@components/Main";
 
 import {
   Alert,
